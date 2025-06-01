739f70046ec4a3d9c0264624e15af938b81665ff
Abhinav Ratan <abhinavratan.dev@gmail.com>
2025-06-01 14:31:27 +0530

    updated

--- gemini_api.py
+++ gemini_api.py
@@ -5,10 +5,5 @@ genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 def get_gemini_response(prompt):
-    response = model.generate_content(
-        prompt,
-        generation_config={
-            "max_tokens": 50000
-        }
-    )
+    response = model.generate_content(prompt)
     return response.text
